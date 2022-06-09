7c8c6ec9262e2ab8ee0f6537596e2df7af798fbc
xgd <1136261471@qq.com>
2022-06-09 10:12:11 +0800

    修改config

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -4,7 +4,7 @@ const sidebar = require('./config/sidebar')
 module.exports = {
   lang: 'zh-CN',
   title: '愿天下没有Bug',
-  description: '这是我的第一个 VuePress 站点',
+  dest: __dirname + '../../../dist',
   theme: defaultTheme({
     // 侧边栏对象
     // 不同子路径下的页面会使用不同的侧边栏
