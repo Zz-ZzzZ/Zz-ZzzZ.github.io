52155520fad9187dbb7b9d04f7b687777527f5e9
Cv-Engineer123 <1136261471@qq.com>
2022-08-01 14:20:41 +0800

    修复BUG(修复logo不正确显示):

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -4,7 +4,7 @@ import navbar from "./config/navbar";
 export default {
   lang: 'zh-CN',
   title: 'Cv-Engineer123',
-  head: [['link', { rel: 'icon', type: 'image/svg+xml', href: './logo.svg' }]],
+  head: [['link', { rel: 'icon', type: 'image/svg+xml', href: '/logo.svg' }]],
   themeConfig: {
     logo: '/logo.svg',
     sidebar,
