774d69d4b1373753026f7a11b12ce750fadb8ed6
Cv-Engineer123 <1136261471@qq.com>
2022-08-02 10:58:10 +0800

    配置变化(主页迁移):

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -5,11 +5,11 @@ import {defineConfig} from "vitepress";
 export default defineConfig({
   lang: 'zh-CN',
   title: 'Cv-Engineer123',
+  lastUpdated: true,
   head: [['link', {rel: 'icon', type: 'image/svg+xml', href: '/logo.svg'}]],
   themeConfig: {
     logo: '/logo.svg',
     sidebar,
     nav: navbar,
-    lastUpdatedText: 'Updated Date'
   },
 })
